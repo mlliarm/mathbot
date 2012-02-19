5921d29602041f89f80f1ac024186bcbe40e4a22
Scott Olson <scott@scott-olson.org>
2012-02-18 23:51:39 -0600

    Initial commit. It should work... fingers crossed.

---
+++ README.md
@@ -0,0 +1,18 @@
+Mathbot
+=======
+Mathbot is a little IRC bot that takes LaTeX input and returns a short URL to
+an online rendering of the LaTeX.
+
+Usage
+-----
+``` irc
+<scott> mathbot: \frac{\sqrt{2}}{2}
+<mathbot> scott: http://da.gd/...
+```
+
+That's it!
+
+Gems
+----
+Mathbot uses the 'cinch' and 'patron' gems.
+
---
+++ mathbot.rb
@@ -0,0 +1,37 @@
+require 'cinch'
+require 'cgi'
+require 'patron'
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server   = "irc.tenthbit.net"
+    c.nick     = "mathbot"
+    c.channels = ["#bots"]
+  end
+
+  helpers do
+    def mathtex_url(code)
+      "http://www.forkosh.com/mathtex.cgi?" + CGI.escape(code)
+    end
+
+    def shorten_url(url)
+      sess = Patron::Session.new
+      sess.timeout = 10
+      sess.base_url = "http://da.gd"
+      sess.headers['User-Agent'] = 'mathbot/1.0'
+
+      resp = sess.get("/s?url=#{CGI.escape(url)}&text=1&strip=1")
+
+      return "Could not shorten URL. Oops!" if resp.status >= 400
+
+      resp.body
+    end
+  end
+
+  on :message, /^#{Regexp.escape nick}\S*\s*(.+)$/ do |m, code|
+    m.reply(shorten_url(mathtex_url(code)), true)
+  end
+end
+
+bot.start
+
